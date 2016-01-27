46422066e36a3b3a3cea43f0ff3d62e89f10e7ea
xiaoxiaoyin <1037691912@qq.com>
2016-01-27 10:02:40 +0800

    checkbox

---
+++ checkbox/src/main/java/com/xiaoxiaoyin/checkbox/MainActivity.java
@@ -0,0 +1,18 @@
+package com.xiaoxiaoyin.checkbox;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+/**
+ * Created by xiaoxiaoyin on 16/1/27.
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
